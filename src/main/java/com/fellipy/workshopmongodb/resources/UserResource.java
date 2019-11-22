059d46a7e1e71c86c66d5dff12f456f60bd83293
fellipySaldanha <fellipy.saldanha@gmail.com>
2019-11-22 18:33:33 -0300

    create users

--- src/main/java/com/fellipy/workshopmongodb/resources/UserResource.java
+++ src/main/java/com/fellipy/workshopmongodb/resources/UserResource.java
@@ -1,15 +1,18 @@
 package com.fellipy.workshopmongodb.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 //import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.fellipy.workshopmongodb.domain.User;
 import com.fellipy.workshopmongodb.dto.UserDTO;
@@ -36,5 +39,11 @@ public class UserResource {
 		return ResponseEntity.ok().body(new UserDTO(user));
 	}
 	
-	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody UserDTO userdto){				
+		User user = service.fromDto(userdto);
+		user = service.insert(user);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}		
 }
--- src/main/java/com/fellipy/workshopmongodb/services/UserService.java
+++ src/main/java/com/fellipy/workshopmongodb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.fellipy.workshopmongodb.domain.User;
+import com.fellipy.workshopmongodb.dto.UserDTO;
 import com.fellipy.workshopmongodb.repository.UserRepository;
 
 @Service
@@ -22,5 +23,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+	
+	public User insert(User user) {
+		return repository.insert(user);
+	}
+	
+	public User fromDto(UserDTO userDTO) {
+		return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
 	}
 }
