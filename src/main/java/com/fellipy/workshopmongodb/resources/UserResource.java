aed5f776ffa8cec1d38947de2c21992b4ec4ecd7
fellipySaldanha <fellipy.saldanha@gmail.com>
2019-11-22 18:25:34 -0200

    UserDTO

--- src/main/java/com/fellipy/workshopmongodb/resources/UserResource.java
+++ src/main/java/com/fellipy/workshopmongodb/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.fellipy.workshopmongodb.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fellipy.workshopmongodb.domain.User;
+import com.fellipy.workshopmongodb.dto.UserDTO;
 import com.fellipy.workshopmongodb.services.UserService;
 
 @RestController
@@ -21,9 +23,10 @@ public class UserResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
 	//@GetMapping
-	public ResponseEntity<List<User>> findAll(){		
-		List<User> list = service.findAll();		
-		return ResponseEntity.ok().body(list);
+	public ResponseEntity<List<UserDTO>> findAll(){		
+		List<User> list = service.findAll();	
+		List<UserDTO> listDto = list.stream().map(user -> new UserDTO(user)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 	
 }
