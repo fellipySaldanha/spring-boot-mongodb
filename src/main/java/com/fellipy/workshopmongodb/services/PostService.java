1fdfdab114b05de58735ff4471f50e831166a3d7
fellipySaldanha <fellipy.saldanha@gmail.com>
2019-11-28 18:35:12 -0200

    Get post by id

---
+++ src/main/java/com/fellipy/workshopmongodb/services/PostService.java
@@ -0,0 +1,27 @@
+package com.fellipy.workshopmongodb.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fellipy.workshopmongodb.domain.Post;
+import com.fellipy.workshopmongodb.repository.PostRepository;
+
+@Service
+public class PostService {
+		
+	@Autowired
+	private PostRepository repository;
+	
+	public List<Post> findAll(){
+		return repository.findAll();
+	}
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+		
+}
